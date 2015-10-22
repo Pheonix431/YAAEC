95076bcb8af1d44a0fff047e8ff8fe6ab3456c29
MD Huq <samiulg3@gmail.com>
2015-10-22 15:53:10 -0400

    added user schema

---
+++ models/User.js
@@ -0,0 +1,18 @@
+var mongoose = require("mongoose");
+
+var UserSchema = new mongoose.Schema({
+  name: { first : String, last: String },
+  local : {
+    username: String,
+    email: String,
+    password: String,
+  },
+  active: { type: Boolean, default: false },
+  phone: Number,
+  card : {} // don't worry about this, this is jus
+});
+
+
+mongoose.model("User", UserSchema);
+
+
