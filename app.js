e0ef4ca5d2a459f1adf7708ba41ef0410aa4dcaa
MD Huq <samiulg3@gmail.com>
2015-10-28 19:54:50 -0400

    added middleware file

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require("mongoose");
+var common = require("./common");
+var config = common.config();
 
 var User = mongoose.model("User");
 
